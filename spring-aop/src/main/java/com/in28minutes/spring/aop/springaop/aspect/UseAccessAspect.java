495477adefc250ba4ba4e0d2fc15331fa2c14016
PedroHenrique2077 <pedrozinho.herique11@gmail.com>
2024-03-19 20:28:16 -0300

    Adicionando o código de estudo ate a secao 9

---
+++ spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/aspect/UseAccessAspect.java
@@ -0,0 +1,24 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//AOP
+//Configuration
+@Aspect
+@Configuration
+public class UseAccessAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution()")
+    public void before(JoinPoint joinPoint){
+        //Advice
+        logger.info(" Check for user access ");
+        logger.info(" Allowed execution for {}", joinPoint);
+    }
+}
