487602bec8b969cdcb830ba23a432d0d8db6962b
PedroHenrique2077 <pedrozinho.herique11@gmail.com>
2024-03-21 12:48:42 -0300

    Adicionando o código de estudo ate a secao 11

---
+++ basic-web-application/src/test/java/com/in28minutes/basicwebapplication/BasicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.basicwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
